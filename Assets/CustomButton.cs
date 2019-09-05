3e06d78a5c82991c1f81275422624252a615bc31
朱海威 <18516902057@163.com>
2019-09-05 23:52:02 +0800

    提交工程

---
+++ Assets/CustomButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class CustomButton :  Button{
+  
+    [System.Serializable]
+    public class CustomButtonClickedEvent : UnityEvent<Button>
+    {
+        
+    }
+    [SerializeField]
+    public new CustomButtonClickedEvent onClick;
+    // Use this for initialization
+    void Start () {
+        base.onClick.AddListener(() => { this.onClick.Invoke(this); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
---
+++ Assets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(CustomButton))]
+public class CustomButtonEditor : Editor {
+
+    SerializedProperty onClick;
+    float value;
+    private void OnEnable()
+    {
+        this.onClick = serializedObject.FindProperty("onClick");
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        //EditorGUILayout.PropertyField(onClick);
+
+        serializedObject.ApplyModifiedProperties();
+
+        value  = EditorGUILayout.Slider("生命值",value, 0, 100);
+        EditorGUI.ProgressBar(GUILayoutUtility.GetRect(20, 20), value/100, "生命值:"+value);
+
+    }
+}
